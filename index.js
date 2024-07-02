da6d5d36e081aa54d809e1d103ae37cb6bd29eda
Mr-kings042 <okohkings042@gmail.com>
2024-07-02 10:30:04 -0700

    complete

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ console.log("clientIp:",clientIp);
 
 try {
       // Get location based on IP
-    const geoResponse = await axios.get(`${LOCATION_API_URL}?apiKey=${LOCATION_API_KEY}`);
+    const geoResponse = await axios.get(`${LOCATION_API_URL}?apiKey=${LOCATION_API_KEY}&field=geo`);
     console.log("geoResponse:",geoResponse.data);
 
     const geoData = geoResponse.data;
