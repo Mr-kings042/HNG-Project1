215a64e7b5932c7f9e893360e4dac49668c78ebf
Mr-kings042 <okohkings042@gmail.com>
2024-07-02 15:30:07 -0700

    done

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const WEATHER_API_URL = process.env.WEATHER_API_URL;
 
 app.get('/', async (req, res) => {
     const visitorName = req.query.visitor_name || 'Dear';
-    const clientIp = req.headers['x-forwarded-for'];
+    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
    
 console.log("clientIp:",clientIp);
    
